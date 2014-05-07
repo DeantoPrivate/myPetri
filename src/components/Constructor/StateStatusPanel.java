aefd89f49b2ff6c082cc7ab71a414255be03e0ca
deanto <lqopl@mail.ru>
2014-05-07 22:28:14 +0400

    7\5 smth related to status panels

--- src/components/Constructor/StateStatusPanel.java
+++ src/components/Constructor/StateStatusPanel.java
@@ -1,5 +1,9 @@
 package components.Constructor;
 
+import core.State;
+import net.staticNet.StateWrap;
+
+import javax.swing.*;
 import java.awt.*;
 
 /**
@@ -9,13 +13,17 @@ public class StateStatusPanel extends StatusPanel {
 
     public static String StatusPanel = "StateStatusPanel";
 
-
-    public StateStatusPanel(){
-
-        setBounds(800,0,480,800);
-        setBackground(Color.RED);
-
+    public static StateWrap _stateWrap;
+    public static void setState(StateWrap stateWrap){
+        _stateWrap = stateWrap;
     }
 
+    @Override
+    public void Init() {
+        super.Init();
 
+        JButton a = new JButton("sd");
+        a.setBounds(100,100,20,20);
+        add(a);
+    }
 }
